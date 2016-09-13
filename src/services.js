748778da03f48839826b889f4e22858b8a197e8e
william mantly <wmantly@gmail.com>
2016-09-13 13:18:59 -0400

    added services

---
+++ src/services.js
@@ -0,0 +1,14 @@
+spa.services = {};
+spa.Service = ( function(){
+	var service = Object.create(spa.__EventBase);
+
+	service.add = function(service){
+		if(!service.name) return false;
+
+		service = this.__declare(service);
+
+		spa.services[service.name] = service;
+	};
+
+	return service;
+} )();
