fe6dd5d412eca7d16b61dc9f9bfb294f302cf993
Thomas Harvey <thomasharvey24@gmail.com>
2016-09-27 10:41:20 -0400

    form component mock up

--- src/component.js
+++ src/component.js
@@ -2,7 +2,7 @@ spa.components = {};
 spa.Component = ( function(){
 	var component = Object.create(spa.RenderBase);
 
-	component.add =function(component){
+	component.add = function(component){
 		if(!component.name) return false;
 
 		component = this.__declare(component);
@@ -11,4 +11,21 @@ spa.Component = ( function(){
 
 
 	return component;
+} )();
+
+spa.Form = ( function(){
+    var form = Object.create(spa.Component);
+
+    form.value_map = {};
+
+    // Does this make sense
+    form.input_cache = {};
+
+    form.renderErrors = function(data){
+        // data is an object where the kwys
+        // are fields. The values are explanations 
+        // as to why the field is invalid.
+    };
+
+    return form;
 } )();
