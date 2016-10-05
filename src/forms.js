88ec4a5f8a8e36b29620a3e9f528167cc4423c9d
Thomas Harvey <thomasharvey24@gmail.com>
2016-10-05 18:02:17 -0400

    updating form error rendering

--- src/forms.js
+++ src/forms.js
@@ -1,16 +1,18 @@
 spa.Form = ( function(){
     var Form = {};
 
+    Form.errorMessageClass = "input-error-message";
+
     Form.add = function(blue_print_config){
         if(!blue_print_config.name) return false;
         spa.components[blue_print_config.name] = function(config){
-            return this.create($.extend(Object.create(blue_print_config), config));
+            return this.create(jQuery.extend(Object.create(blue_print_config), config));
         }.bind(this);
     };
 
     Form.renderErrors = function(data){
         var that = this;
-
+        that.$container.find("." + that.errorMessageClass).remove();
         jQuery.each(data, function(key, value){
             if (Object.hasOwnProperty.call(that, "set" + key + "Error")){
                 that["set" + key + "Error"](key, value)
@@ -22,13 +24,14 @@ spa.Form = ( function(){
     };
 
     Form.setError = function(name, message){
+        var that = this;
         var $target = this.getErrorTarget(name);
         if (message.prototype !== Array.prototype){
             message = [message];
         }
         jQuery.each(message, function(idx, value){
-            $target.append("<span>" + value + "</span>");
-        });
+            $target.append('<span class="' + that.errorMessageClass + '">' + value + '</span>');
+        }this);
     };
 
     Form.getErrorTarget = function(name) {
