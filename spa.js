f7b5ebb66e8d15bb269e2659d9717ab2177c12e0
Thomas Harvey <thomasharvey24@gmail.com>
2016-11-07 12:55:33 -0500

    adding build

--- spa.js
+++ spa.js
@@ -405,12 +405,12 @@ spa.Model = ( function(){
 /* 
 	error templates
 */
-spa.RenderBase.errorTemplates['404'] = spa.RenderBase.constructor({
+spa.RenderBase.errorTemplates['404'] = spa.RenderBase.create({
 	template: '<center><h1><i>Page {{name}} Not Found</i></h1>{{message}}<center>',
 	title: '404'
 });
 
-spa.RenderBase.errorTemplates['500'] = spa.RenderBase.constructor({
+spa.RenderBase.errorTemplates['500'] = spa.RenderBase.create({
 	title: '500',
 	template: '<center><h1><i>SPA error {{name}}</i></h1>{{message}}<center>'
 });
