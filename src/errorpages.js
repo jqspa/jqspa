befb5920d233e0136df26a6f2f587112d4e0af79
Thomas Harvey <thomasharvey24@gmail.com>
2016-11-07 12:52:36 -0500

    fixed error templates

--- src/errorpages.js
+++ src/errorpages.js
@@ -1,9 +1,9 @@
-spa.RenderBase.errorTemplates['404'] = spa.RenderBase.constructor({
+spa.RenderBase.errorTemplates['404'] = spa.RenderBase.create({
 	template: '<center><h1><i>Page {{name}} Not Found</i></h1>{{message}}<center>',
 	title: '404'
 });
 
-spa.RenderBase.errorTemplates['500'] = spa.RenderBase.constructor({
+spa.RenderBase.errorTemplates['500'] = spa.RenderBase.create({
 	title: '500',
 	template: '<center><h1><i>SPA error {{name}}</i></h1>{{message}}<center>'
 });
