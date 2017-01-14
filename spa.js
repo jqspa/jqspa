00e60553d59288eadb564aa48ecb490a6a092c8d
william mantly <wmantly@gmail.com>
2017-01-14 00:20:53 -0500

    updated build

--- spa.js
+++ spa.js
@@ -156,6 +156,12 @@ spa.utils = {};
         }
         return obj;
     };
+    
+    $.getCookie = function(){
+        var value = "; " + document.cookie;
+        var parts = value.split("; " + name + "=");
+        if (parts.length == 2) return parts.pop().split(";").shift();
+    };
 })(jQuery);
 
 /*
