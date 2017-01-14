ccdab429485f35f6c1a4e2eaadac435fd70b550f
william mantly <wmantly@gmail.com>
2017-01-14 00:20:51 -0500

    added get cookie function

--- src/utils.js
+++ src/utils.js
@@ -32,4 +32,10 @@ spa.utils = {};
         }
         return obj;
     };
+    
+    $.getCookie = function(){
+        var value = "; " + document.cookie;
+        var parts = value.split("; " + name + "=");
+        if (parts.length == 2) return parts.pop().split(";").shift();
+    };
 })(jQuery);
