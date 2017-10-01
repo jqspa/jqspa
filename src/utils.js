512b52624262e457e9a0f79e4b99429b1d48edd3
William Mantly <wmantly@gmail.com>
2017-10-01 18:35:06 -0400

    fixed UUID utility

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,13 @@
 spa.utils = {};
 
 (function(utils) {
+
+    // only use memory for one empty function
 	utils.emptyFunc = function(){};
 
 	utils.UUID = function(){
         var d = new Date().getTime();
-        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        return uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
             var r = (d + Math.random()*16)%16 | 0;
             d = Math.floor(d/16);
             return (c=='x' ? r : (r&0x3|0x8)).toString(16);
@@ -31,11 +33,5 @@ spa.utils = {};
             }
         }
         return obj;
-    };
-    
-    $.getCookie = function(){
-        var value = "; " + document.cookie;
-        var parts = value.split("; " + name + "=");
-        if (parts.length == 2) return parts.pop().split(";").shift();
     };
 })(jQuery);
